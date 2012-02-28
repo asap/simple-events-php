c8d53d235741190230f97736437f6f5c0d1c167a
ASAP <asap@briteideas.com>
2012-02-28 09:03:57 -0500

    [ALX] - Simplifying constants

--- admin/includes/constants.php
+++ admin/includes/constants.php
@@ -6,19 +6,3 @@ define('DB_SERVER', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_NAME', 'events');
-
-// Staging
-/*
-	define('DB_SERVER', 'localhost');
-	define('DB_USER', 'briteid_alethea');
-	define('DB_PASSWORD', 'lTnPS8TuZ-y$');
-	define('DB_NAME', 'briteid_alethea');
-*/
-
-// Production
-/*
-	define('DB_SERVER', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASSWORD', 'root');
-	define('DB_NAME', 'events');
-*/
